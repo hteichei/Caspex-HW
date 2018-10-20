b692d39eede9940cc529aa8779e9440a9f7573f7
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-20 14:44:14 -0700

    Price Summary Component to display pricing details

---
+++ src/Components/PriceSummary.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class PriceSummary extends Component {
+  render() {
+    const { subTotal, pickupSavings, taxes, total } = this.props;
+    return <div />;
+  }
+}
+
+export default PriceSummary;
