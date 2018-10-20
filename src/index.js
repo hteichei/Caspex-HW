d19723710db4870ca895850514c659c5a38f82f3
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-20 11:08:16 -0700

    Added redux set up back to index.js file and deleted default react logo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore } from 'Redux';
-// import rootReducer from './rootReducer';
-// import { composeWithDevTools } from 'Redux-devtools-extension';
-// import { Provider } from 'react-redux';
+import { createStore } from 'Redux';
+import rootReducer from './store/reducers/rootReducer';
+import { composeWithDevTools } from 'Redux-devtools-extension';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
