04a186990d434d99afca860a1c8fd5a74e86b682
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-20 16:07:35 -0700

    Rendering PriceSummary Component in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 // import './App.css';
 import Expandable from './Components/Expandable';
+import PriceSummary from './Containers/PriceSummaryContainer';
 
 class App extends Component {
   render() {
@@ -10,24 +10,10 @@ class App extends Component {
         <Expandable showTitle="See Item Details" hideTitle="Hide Item Details">
           <p>Essentials</p>
         </Expandable>
+        <PriceSummary />
       </div>
     );
   }
 }
 
 export default App;
-
-/*
-{
-  promoApplied: false,
-  items: [
-    {
-      title: '',
-      price: '',
-      description: '',
-      img: '',
-    }
-]
-
-}
-*/
