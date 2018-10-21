c0a39afdaa4fe0cf5dbaeb977153ea51e935665d
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-20 17:07:20 -0700

    Controlled form for applying promotion

---
+++ src/Components/PromoCodeForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PromoCodeForm extends Component {
+  render() {
+    return (
+      <div>
+        <form action="">
+          <div>
+            <label>Promo Code</label>
+          </div>
+          <input type="text" name="promo" />
+        </form>
+      </div>
+    );
+  }
+}
