b5f5c785ab883e5400656d8af95e1ae1bae3b63b
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-20 17:05:24 -0700

    Created CheckoutCard Component to contain and render all other components

---
+++ src/Components/CheckoutCard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Expandable from './Expandable';
+import PriceSummary from '../Containers/PriceSummaryContainer';
+import ItemDetailsCard from './ItemDetailsCard';
+
+class CheckoutCard extends Component {
+  render() {
+    const { items, currency } = this.props;
+    return (
+      <div>
+        <PriceSummary />
+        <Expandable showTitle="See Item Details" hideTitle="Hide Item Details">
+          {items.map((item, idx) => (
+            <div key={idx}>
+              <ItemDetailsCard
+                title={item.title}
+                img={item.img}
+                quantity={item.quantity}
+                currency={currency}
+                price={item.price}
+                shippingCost={item.shippingCost}
+              />
+              <hr />
+            </div>
+          ))}
+        </Expandable>
+        <Expandable showTitle="Apply Promo Code" hideTitle="Hide Promo Code">
+          <div />
+        </Expandable>
+      </div>
+    );
+  }
+}
+
+export default CheckoutCard;
