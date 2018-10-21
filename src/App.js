9538d8d7ff1f14a63927117eea88f4c72cbec119
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-20 17:08:52 -0700

    App now renders CheckoutCard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-// import './App.css';
-import Expandable from './Components/Expandable';
-import PriceSummary from './Containers/PriceSummaryContainer';
+import CheckoutCard from './Containers/CheckoutCardContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Expandable showTitle="See Item Details" hideTitle="Hide Item Details">
-          <p>Essentials</p>
-        </Expandable>
-        <PriceSummary />
+        <CheckoutCard />
       </div>
     );
   }
