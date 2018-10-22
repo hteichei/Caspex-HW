29f9140b613546c7202a36e1177aa1879c2eb1fe
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-21 19:07:16 -0700

    Added styling and unit test for Tooltip component

---
+++ src/Components/Tooltip.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Tooltip from './Tooltip';
+
+describe('<Tooltip />', () => {
+  const wrapper = shallow(
+    <Tooltip>
+      <span className="tooltip">
+        Picking up your order in store helps cut costs.
+      </span>
+      <span className="title">Pickup Savings</span>
+    </Tooltip>
+  );
+
+  it('renders the title', () => {
+    console.log(wrapper.debug());
+    expect(wrapper.find('.title').text()).toEqual('Pickup Savings');
+  });
+  it('renders tip component on mouse hover', () => {
+    console.log(wrapper.debug());
+    wrapper.find('.title').simulate('mouseOver');
+    expect(wrapper.find('.tooltip').text()).toEqual(
+      'Picking up your order in store helps cut costs.'
+    );
+  });
+});
